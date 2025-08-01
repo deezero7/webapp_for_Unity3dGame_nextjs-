6ba39eb01383e742f5c529585adf171aee9d248a
Deepndra singh <46988044+deezero7@users.noreply.github.com>
2025-08-02 05:15:25 +0530

    forgotpassword

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
 
   // basepath is used for deploying to a github pages , when building app for github pages as static files use it.
   // for local development, you can ignore this or comment it out
-  basePath: "/webapp_for_Unity3dGame_nextjs-",
+  // basePath: "/webapp_for_Unity3dGame_nextjs-",
 };
 
 export default nextConfig;
